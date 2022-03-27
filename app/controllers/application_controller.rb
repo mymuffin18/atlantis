8591b7b509617826b5421ea931c7608812744098
Josh <josh.uy25@gmail.com>
2022-03-27 14:32:19 +0800

    adds image url for profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   has_one_attached :profile_pic
 
-  after_commit :add_default_profile_pic, on: [:create, :update]
-
-
+  after_commit :add_default_profile_pic, on: %i[create update]
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -14,9 +12,11 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
 
+  def profile_pic_url
+    url_for(profile_pic)
+  end
+
   def add_default_profile_pic
-    unless profile_pic.attached?
-      self.profile_pic.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-profile.jpg')), filename: 'default-profile.jpg', content_type: 'image/jpg')
-    end
+    profile_pic.attach(io: File.open('app/assets/images/default-profile.jpg'), filename: 'default-profile.jpg', content_type: 'image/jpg') unless profile_pic.attached?
   end
 end
