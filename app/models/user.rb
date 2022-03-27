ac9b633bf4aaf33845882c2040876c506cc43252
Josh <josh.uy25@gmail.com>
2022-03-27 14:10:24 +0800

    added registration controller adds default image for newly registered users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,20 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   has_one_attached :profile_pic
 
+  after_commit :add_default_profile_pic, on: [:create, :update]
+
+
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+
+  def add_default_profile_pic
+    unless profile_pic.attached?
+      self.profile_pic.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'default-profile.jpg')), filename: 'default-profile.jpg', content_type: 'image/jpg')
+    end
+  end
 end
