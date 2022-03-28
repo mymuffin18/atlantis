537fa661770838d5865af49ccbd501c2cd6a59dd
Josh <josh.uy25@gmail.com>
2022-03-28 23:42:41 +0800

    adds disaster_report route

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     namespace :v1 do
     resources :disasters, only: [:index, :show, :create, :update, :destroy]
     resources :members, only: [:index, :show, :update]
+    resources :disaster_reports, only: [:index, :create]
     end
   end
 end
