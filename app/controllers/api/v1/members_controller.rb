62c5786ead887c2664f1228c3df390b8cfd407e7
Josh <josh.uy25@gmail.com>
2022-03-28 19:34:11 +0800

    fixed rubocop offenses

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -1,24 +1,22 @@
 module Api
   module V1
-  
     class MembersController < ApplicationController
       before_action :authenticate_user!
-    
+
       def index
         user = current_user
-    
+
         render json: serialize_user(user), status: :ok
       end
-    
-    
+
       def show
         user = User.find(params[:id])
-    
+
         render json: serialize_user(user), status: :ok
       end
-    
-      
+
       private
+
       def serialize_user(user)
         {
           id: user.id,
--- spec/requests/members_spec.rb
+++ spec/requests/members_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 require 'devise/jwt/test_helpers'
 
-RSpec.describe "Members", type: :request do
+RSpec.describe 'Members', type: :request do
   let(:auth_headers) do
     user = create(:user)
     headers = { 'Accept' => 'application/json', 'Content-Type' => 'application/json' }
@@ -9,20 +9,19 @@ RSpec.describe "Members", type: :request do
     Devise::JWT::TestHelpers.auth_headers(headers, user)
   end
 
-  
-  describe "GET /members" do
+  describe 'GET /members' do
     it 'returns the logged in user' do
       get '/api/v1/members', headers: auth_headers
       expect(response).to have_http_status(:ok)
     end
   end
 
-  describe "GET /members/:id" do
-    let!(:user) { User.create(first_name: 'first', last_name: 'last', email: 'first@example.com', password: '123456')}
+  describe 'GET /members/:id' do
+    let!(:user) { User.create(first_name: 'first', last_name: 'last', email: 'first@example.com', password: '123456') }
+
     it 'returns a user' do
       get "/api/v1/members/#{user.id}", headers: auth_headers
       expect(response).to have_http_status(:ok)
     end
   end
-  
 end
