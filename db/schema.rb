3a49727f4f3a56280dd1032dfdcc3410f50e28ae
Keith Pineda <keith.m.pineda@gmail.com>
2022-03-29 22:14:44 +0800

    crud on locations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_27_122905) do
+ActiveRecord::Schema.define(version: 2022_03_28_144143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,16 @@ ActiveRecord::Schema.define(version: 2022_03_27_122905) do
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.float "longitude"
+    t.float "latitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "locname"
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -85,4 +95,5 @@ ActiveRecord::Schema.define(version: 2022_03_27_122905) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "locations", "users"
 end
