a9415334eac197fc1c2f8dee127b69d3d0780cf3
Josh <josh.uy25@gmail.com>
2022-04-10 12:50:04 +0800

    remove locations add seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_174012) do
+ActiveRecord::Schema.define(version: 2022_04_10_043933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,16 +85,6 @@ ActiveRecord::Schema.define(version: 2022_04_09_174012) do
     t.index ["jti"], name: "index_jwt_denylist_on_jti"
   end
 
-  create_table "locations", force: :cascade do |t|
-    t.float "longitude"
-    t.float "latitude"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "locname"
-    t.index ["user_id"], name: "index_locations_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -105,7 +95,6 @@ ActiveRecord::Schema.define(version: 2022_04_09_174012) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "fieldname"
     t.integer "warning", default: 0
     t.date "dateOfSuspension"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -132,5 +121,4 @@ ActiveRecord::Schema.define(version: 2022_04_09_174012) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "disaster_reports", "disasters"
   add_foreign_key "disaster_reports", "users"
-  add_foreign_key "locations", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Admin.create(first_name: 'admin', last_name: 'admin', email: 'admin@admin.com', password:'123456')
+
+d1 = Disaster.new(disaster_type: 'flood')
+d1.avatar.attach(io: File.open('app/assets/images/flood-icon.png'), filename: 'flood-icon.png', content_type: 'image/png')
+d1.save
+
+d2 = Disaster.new(disaster_type: 'landslide')
+d2.avatar.attach(io: File.open('app/assets/images/landslide-icon.png'), filename: 'landslide-icon.png', content_type: 'image/png')
+d2.save
+
+d3 = Disaster.new(disaster_type: 'tornado')
+d3.avatar.attach(io: File.open('app/assets/images/tornado-icon.png'), filename: 'tornado-icon.png', content_type: 'image/png')
+d3.save
+
+d4 = Disaster.new(disaster_type: 'tsunami')
+d4.avatar.attach(io: File.open('app/assets/images/tsunami-icon.png'), filename: 'tsunami-icon.png', content_type: 'image/png')
+d4.save
