de43ea6d63ec0155f514792952f98d3841fb4a30
Josh <josh.uy25@gmail.com>
2022-04-10 12:54:38 +0800

    remove specs and routes for locations

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
     namespace :v1 do
     resources :disasters, only: [:index, :show, :create, :update, :destroy]
     resources :members, only: [:index, :show, :update]
-    resources :locations, only: [:index, :show, :create, :update, :destroy]
     resources :disaster_reports, only: [:index, :create, :show, :update, :destroy]
     get 'pending_reports/', to: 'disaster_reports#pending_reports'
     put 'disaster_reports/:id/vote', to: 'disaster_reports#vote'
